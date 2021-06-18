787497463a432758ad2f748ede2055e50e3af994
Jakub Baranski <kubabar1@interia.pl>
2021-06-18 20:13:53 +0200

    Fix some problems with generation 3d coordinates utils

--- dataset_utils/process_data.py
+++ dataset_utils/process_data.py
@@ -1,10 +1,12 @@
 import os
+import cv2
 import sys
 import pathlib
 import csv
 import shutil
 from configparser import ConfigParser
 import numpy as np
+import importlib
 
 sys.path.append(os.path.abspath(os.path.join(os.path.curdir, '..')))
 
@@ -28,51 +30,69 @@ def process_berkeley_mhad_to_2d_single_person(silhouettes_path, results_path):
 
 
 def process_berkeley_mhad_to_3d_single_person(silhouettes_path, results_path, frame_width, frame_height, video_pose_3d_path):
-    sys.path.append(video_pose_3d_path)
-    from run_video_pose_3d import process_custom_2d_to_3d
-    # from run_video_pose_3d import process_custom_2d_npz_to_3d
+    tmp_3d_results_path = os.path.join(results_path, 'tmp')
     if os.path.exists(results_path):
         shutil.rmtree(results_path)
-    keypoints_npz_path = '/home/kuba/workspace/human_action_recognition/VideoPose3D/data/data_2d_custom_kinect2.npz'
-    process_custom_2d_to_3d(keypoints_npz_path, video_pose_3d_path,
-                            '/home/kuba/workspace/human_action_recognition/VideoPose3D/kinect2', 'kinect2', 'custom', viz_output=None)
-    # for subject_id, subject_path in enumerate(sorted([f.path for f in os.scandir(silhouettes_path)])):
-    #     for action_dir_path in sorted([f.path for f in os.scandir(subject_path)]):
-    #         for repetition_id, repetition_path in enumerate(sorted([f.path for f in os.scandir(action_dir_path)])):
-    #             pr = os.path.join(repetition_path, 'pose_results')
-    #             # br = os.path.join(repetition_path, 'bbox')
-    #             res = os.path.join(results_path, '/'.join(repetition_path.split('/')[1:]), 'VideoPose3D')
-    #             if not os.path.exists(res):
-    #                 pathlib.Path(os.path.dirname(res)).mkdir(parents=True)
-    #             final_poses = np.array(
-    #                 [
-    #                     np.array([np.array([i[0], i[1]], dtype='float32')
-    #                               for i in np.array_split(frame_poses[0], 17)], dtype='float32')
-    #                     if len(frame_poses)
-    #                     else np.zeros((17, 2), dtype='float32')
-    #                     for frame_poses
-    #                     in get_keypoints_for_all_frames(pr)], dtype='float32')
-    #             # final_bboxes = [poses[0] if len(poses) else [] for poses in get_bbox_for_all_frames(br)]
-    #             np.savez(res,
-    #                      positions_2d={
-    #                          'VideoPose3D': {
-    #                              'custom': [
-    #                                  final_poses
-    #                              ]
-    #                          }
-    #                      }, metadata={
-    #                     'layout_name': 'coco',
-    #                     'num_joints': 17,
-    #                     'keypoints_symmetry': [[1, 3, 5, 7, 9, 11, 13, 15], [2, 4, 6, 8, 10, 12, 14, 16]],
-    #                     'video_metadata': {
-    #                         'VideoPose3D': {
-    #                             'w': frame_width,
-    #                             'h': frame_height
-    #                         }
-    #                     }
-    #                 })
-    # qwe = '/home/kuba/workspace/human_action_recognition/HumanActionRecognition/har_implementations/pose_based_har_framework/data/videopose3d/s01/a01/r01/VideoPose3D.npz'
-    # process_custom_2d_npz_to_3d('checkpoint', video_pose_3d_path, qwe)
+
+    viz_subject = 'VideoPose3D'
+    viz_action = 'custom'
+
+    output_file_name = '3d_coordinates'
+
+    for subject_id, subject_path in enumerate(sorted([f.path for f in os.scandir(silhouettes_path)])):
+        for action_dir_path in sorted([f.path for f in os.scandir(subject_path)]):
+            for repetition_id, repetition_path in enumerate(sorted([f.path for f in os.scandir(action_dir_path)])):
+                pr = os.path.join(repetition_path, 'pose_results')
+                res = os.path.join(tmp_3d_results_path, '/'.join(repetition_path.split('/')[1:]), viz_subject)
+                if not os.path.exists(res):
+                    pathlib.Path(os.path.dirname(res)).mkdir(parents=True)
+                final_poses = np.array(
+                    [
+                        np.array([np.array([i[0], i[1]], dtype='float32')
+                                  for i in np.array_split(frame_poses[0], 17)], dtype='float32')
+                        if len(frame_poses)
+                        else np.zeros((17, 2), dtype='float32')
+                        for frame_poses
+                        in get_keypoints_for_all_frames(pr)], dtype='float32')
+                np.savez(res,
+                         positions_2d={
+                             viz_subject: {
+                                 viz_action: [
+                                     final_poses
+                                 ]
+                             }
+                         }, metadata={
+                        'layout_name': 'coco',
+                        'num_joints': 17,
+                        'keypoints_symmetry': [[1, 3, 5, 7, 9, 11, 13, 15], [2, 4, 6, 8, 10, 12, 14, 16]],
+                        'video_metadata': {
+                            viz_subject: {
+                                'w': frame_width,
+                                'h': frame_height
+                            }
+                        }
+                    })
+
+    sys.path.append(video_pose_3d_path)
+    from run_video_pose_3d import process_custom_2d_to_3d_npz
+
+    for subject_path, subject_name in sorted([(f.path, f.name) for f in os.scandir(tmp_3d_results_path)]):
+        for action_dir_path, action_name in sorted([(f.path, f.name) for f in os.scandir(subject_path)]):
+            for repetition_path, repetition_name in sorted([(f.path, f.name) for f in os.scandir(action_dir_path)]):
+                pose_2d_npz_path = os.path.join(repetition_path, viz_subject + '.npz')
+                output_path = os.path.join(results_path, subject_name, action_name, repetition_name, output_file_name)
+                if not os.path.exists(output_path):
+                    pathlib.Path(os.path.dirname(output_path)).mkdir(parents=True)
+                # print('#######################################################################')
+                # print(pose_2d_npz_path)
+                # print(viz_subject)
+                # print(viz_action)
+                # print(output_path)
+                # print('#######################################################################')
+                process_custom_2d_to_3d_npz(pose_2d_npz_path, video_pose_3d_path, viz_subject, viz_action, viz_export=output_path)
+
+    if os.path.exists(tmp_3d_results_path):
+        shutil.rmtree(tmp_3d_results_path)
 
 
 def main():
@@ -86,37 +106,34 @@ def main():
     berkeley_frame_height = 480
 
     # process_berkeley_mhad_to_2d_single_person(silhouettes_path, results_path_2d)
-    process_berkeley_mhad_to_3d_single_person(silhouettes_path, results_path_3d, berkeley_frame_width, berkeley_frame_height,
-                                              video_pose_3d_path)
-
-    # pr = '/home/kuba/workspace/pose_estimation/vis_results/kinect/top_down/hrnet_w48_coco_384x288_dark/pose_results'
-    # final_poses = np.array(
-    #     [
-    #         np.array([np.array([i[0], i[1]], dtype='float32')
-    #                   for i in np.array_split(frame_poses[0], 17)], dtype='float32')
-    #         if len(frame_poses)
-    #         else np.zeros((17, 2), dtype='float32')
-    #         for frame_poses
-    #         in get_keypoints_for_all_frames(pr)], dtype='float32')
-    # # final_bboxes = [poses[0] if len(poses) else [] for poses in get_bbox_for_all_frames(br)]
-    # np.savez('data_2d_custom_kinect2',
-    #          positions_2d={
-    #              'kinect2': {
-    #                  'custom': [
-    #                      final_poses
-    #                  ]
-    #              }
-    #          }, metadata={
-    #         'layout_name': 'coco',
-    #         'num_joints': 17,
-    #         'keypoints_symmetry': [[1, 3, 5, 7, 9, 11, 13, 15], [2, 4, 6, 8, 10, 12, 14, 16]],
-    #         'video_metadata': {
-    #             'kinect2': {
-    #                 'w': berkeley_frame_width,
-    #                 'h': berkeley_frame_height
-    #             }
-    #         }
-    #     })
+    # process_berkeley_mhad_to_3d_single_person(silhouettes_path, results_path_3d, berkeley_frame_width, berkeley_frame_height,
+    #                                           video_pose_3d_path)
+    vid_ref = '/home/kuba/workspace/human_action_recognition/datasets/BerkeleyMHAD/Camera/Cluster01/Cam01/S01/A02/R01.mp4'
+    poses_keypoints = np.load(
+        '/home/kuba/workspace/human_action_recognition/HumanActionRecognition/datasets/berkeley_mhad/3d/s01/a02/r01/3d_coordinates.npy')
+    cap = cv2.VideoCapture(vid_ref)
+    frame_id = 0
+    stop = False
+    while True:
+        if not stop:
+            ret, frame = cap.read()
+            if ret:
+                for i in range(17):
+                    x = int(float(poses_keypoints[frame_id][i][0] * (berkeley_frame_width / 2) + berkeley_frame_width / 2))
+                    y = int(float(poses_keypoints[frame_id][i][1] * (berkeley_frame_height / 2) + berkeley_frame_height / 2))
+                    cv2.circle(frame, (x, y), radius=4, color=(0, 0, 255), thickness=-1)
+                frame = cv2.resize(frame, (500, 500))
+                cv2.imshow('frame', frame)
+                frame_id += 1
+            else:
+                break
+        k = cv2.waitKey(25) & 0xFF
+        if k == ord('q'):
+            break
+        if k == ord(' '):
+            stop = not stop
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
--- dataset_utils/run_video_pose_3d.py
+++ dataset_utils/run_video_pose_3d.py
@@ -6,25 +6,23 @@ from common.loss import *
 from common.generators import UnchunkedGenerator
 from common.utils import deterministic_random
 from common.custom_dataset import CustomDataset
+from common.h36m_dataset import h36m_skeleton
 
 
-def process_custom_2d_to_3d(keypoints_npz_path, video_pose_path, viz_video, viz_subject, viz_action, viz_output='output.mp4',
-                            viz_export='output.npz'):
+def process_custom_2d_to_3d_npz(keypoints_npz_path, video_pose_path, viz_subject, viz_action, viz_video=None,
+                                viz_output='output.mp4', viz_export='output'):
+    dataset = CustomDataset(keypoints_npz_path)
     subjects_test = [viz_subject]
     architecture = '3,3,3,3,3'
     dropout = 0.25
     channels = 1024
-    stride = 1
     viz_camera = 0
-    disable_optimizations = None
     dense = None
     causal = None
     filter_widths = [int(x) for x in architecture.split(',')]
     actions = '*'
     action_filter = None if actions == '*' else actions.split(',')
     model_params = 0
-
-    dataset = CustomDataset(keypoints_npz_path)
 
     for subject in dataset.subjects():
         for action in dataset[subject].keys():
@@ -54,7 +52,6 @@ def process_custom_2d_to_3d(keypoints_npz_path, video_pose_path, viz_video, viz_
                 continue
 
             for cam_idx in range(len(keypoints[subject][action])):
-
                 # We check for >= instead of == because some videos in H3.6M contain extra frames
                 mocap_length = dataset[subject][action]['positions_3d'][cam_idx].shape[0]
                 assert keypoints[subject][action][cam_idx].shape[0] >= mocap_length
@@ -73,48 +70,37 @@ def process_custom_2d_to_3d(keypoints_npz_path, video_pose_path, viz_video, viz_
                 kps[..., :2] = normalize_screen_coordinates(kps[..., :2], w=cam['res_w'], h=cam['res_h'])
                 keypoints[subject][action][cam_idx] = kps
 
-    cameras_valid, poses_valid, poses_valid_2d = fetch(subjects_test, keypoints, dataset, action_filter)
+    _, _, poses_valid_2d = fetch(subjects_test, keypoints, dataset, action_filter)
 
-    if not disable_optimizations and not dense and stride == 1:
-        # Use optimized model for single-frame predictions
-        model_pos_train = TemporalModelOptimized1f(poses_valid_2d[0].shape[-2], poses_valid_2d[0].shape[-1],
-                                                   dataset.skeleton().num_joints(), filter_widths=filter_widths, causal=causal,
-                                                   dropout=dropout, channels=channels)
-    else:
-        # When incompatible settings are detected (stride > 1, dense filters, or disabled optimization) fall back to normal model
-        model_pos_train = TemporalModel(poses_valid_2d[0].shape[-2], poses_valid_2d[0].shape[-1], dataset.skeleton().num_joints(),
-                                        filter_widths=filter_widths, causal=causal, dropout=dropout, channels=channels,
-                                        dense=dense)
+    # Use optimized model for single-frame predictions
+    model_pos_train = TemporalModelOptimized1f(poses_valid_2d[0].shape[-2], poses_valid_2d[0].shape[-1],
+                                               dataset.skeleton().num_joints(), filter_widths=filter_widths, causal=causal,
+                                               dropout=dropout, channels=channels)
 
     model_pos = TemporalModel(poses_valid_2d[0].shape[-2], poses_valid_2d[0].shape[-1], dataset.skeleton().num_joints(),
                               filter_widths=filter_widths, causal=causal, dropout=dropout, channels=channels, dense=dense)
 
     receptive_field = model_pos.receptive_field()
-    print('INFO: Receptive field: {} frames'.format(receptive_field))
-    pad = (receptive_field - 1) // 2  # Padding on each side
+    pad = (receptive_field - 1) // 2
+
     if causal:
-        print('INFO: Using causal convolutions')
         causal_shift = pad
     else:
         causal_shift = 0
 
     for parameter in model_pos.parameters():
         model_params += parameter.numel()
-    print('INFO: Trainable parameter count:', model_params)
 
     if torch.cuda.is_available():
         model_pos = model_pos.cuda()
         model_pos_train = model_pos_train.cuda()
 
     chk_filename = os.path.join(video_pose_path, 'checkpoint', 'pretrained_h36m_detectron_coco.bin')
-    print('Loading checkpoint', chk_filename)
     checkpoint = torch.load(chk_filename, map_location=lambda storage, loc: storage)
-    print('This model was trained for {} epochs'.format(checkpoint['epoch']))
     model_pos_train.load_state_dict(checkpoint['model_pos'])
     model_pos.load_state_dict(checkpoint['model_pos'])
 
     if 'model_traj' in checkpoint:
-        # Load trajectory model if it contained in the checkpoint (e.g. for inference in the wild)
         model_traj = TemporalModel(poses_valid_2d[0].shape[-2], poses_valid_2d[0].shape[-1], 1,
                                    filter_widths=filter_widths, causal=causal, dropout=dropout, channels=channels, dense=dense)
         if torch.cuda.is_available():
@@ -123,20 +109,11 @@ def process_custom_2d_to_3d(keypoints_npz_path, video_pose_path, viz_video, viz_
     else:
         model_traj = None
 
-    test_generator = UnchunkedGenerator(cameras_valid, poses_valid, poses_valid_2d,
-                                        pad=pad, causal_shift=causal_shift, augment=False,
-                                        kps_left=kps_left, kps_right=kps_right, joints_left=joints_left,
-                                        joints_right=joints_right)
-    print('INFO: Testing on {} frames'.format(test_generator.num_frames()))
-
-    print('Rendering...')
     input_keypoints = keypoints[viz_subject][viz_action][viz_camera].copy()
     ground_truth = None
     if viz_subject in dataset.subjects() and viz_action in dataset[viz_subject]:
         if 'positions_3d' in dataset[viz_subject][viz_action]:
             ground_truth = dataset[viz_subject][viz_action]['positions_3d'][viz_camera].copy()
-    if ground_truth is None:
-        print('INFO: this action is unlabeled. Ground truth will not be rendered.')
 
     test_time_augmentation = None
     gen = UnchunkedGenerator(None, None, [input_keypoints],
@@ -151,49 +128,54 @@ def process_custom_2d_to_3d(keypoints_npz_path, video_pose_path, viz_video, viz_
     if viz_export is not None:
         np.save(viz_export, prediction)
 
-    if viz_output is not None:
-        if ground_truth is not None:
-            # Reapply trajectory
-            trajectory = ground_truth[:, :1]
-            ground_truth[:, 1:] += trajectory
-            prediction += trajectory
+    if viz_output is not None and viz_video is not None:
+        render_video(dataset, prediction, viz_subject, viz_camera, viz_video, viz_output, input_keypoints, keypoints_metadata,
+                     ground_truth)
 
-        # Invert camera transformation
-        cam = dataset.cameras()[viz_subject][viz_camera]
-        if ground_truth is not None:
-            prediction = camera_to_world(prediction, R=cam['orientation'], t=cam['translation'])
-            ground_truth = camera_to_world(ground_truth, R=cam['orientation'], t=cam['translation'])
-        else:
-            # If the ground truth is not available, take the camera extrinsic params from a random subject.
-            # They are almost the same, and anyway, we only need this for visualization purposes.
-            for subject in dataset.cameras():
-                if 'orientation' in dataset.cameras()[subject][viz_camera]:
-                    rot = dataset.cameras()[subject][viz_camera]['orientation']
-                    break
-            prediction = camera_to_world(prediction, R=rot, t=0)
-            # We don't have the trajectory, but at least we can rebase the height
-            prediction[:, :, 2] -= np.min(prediction[:, :, 2])
+    dataset._skeleton._parents = [-1, 0, 1, 2, 3, 4, 0, 6, 7, 8, 9, 0, 11, 12, 13, 14, 12,
+                                  16, 17, 18, 19, 20, 19, 22, 12, 24, 25, 26, 27, 28, 27, 30]
 
-        anim_output = {'Reconstruction': prediction}
 
-        # if ground_truth is not None and not viz_no_ground_truth:
-        #     anim_output['Ground truth'] = ground_truth
+def render_video(dataset, prediction, viz_subject, viz_camera, viz_video, viz_output, input_keypoints, keypoints_metadata,
+                 ground_truth):
+    viz_limit = -1
+    viz_bitrate = 3000
+    viz_downsample = 1
+    viz_skip = 0
+    viz_size = 5
 
-        input_keypoints = image_coordinates(input_keypoints[..., :2], w=cam['res_w'], h=cam['res_h'])
+    if ground_truth is not None:
+        # Reapply trajectory
+        trajectory = ground_truth[:, :1]
+        ground_truth[:, 1:] += trajectory
+        prediction += trajectory
 
-        from common.visualization import render_animation
+    # Invert camera transformation
+    cam = dataset.cameras()[viz_subject][viz_camera]
+    if ground_truth is not None:
+        prediction = camera_to_world(prediction, R=cam['orientation'], t=cam['translation'])
+    else:
+        # If the ground truth is not available, take the camera extrinsic params from a random subject.
+        # They are almost the same, and anyway, we only need this for visualization purposes.
+        for subject in dataset.cameras():
+            if 'orientation' in dataset.cameras()[subject][viz_camera]:
+                rot = dataset.cameras()[subject][viz_camera]['orientation']
+                break
+        prediction = camera_to_world(prediction, R=rot, t=0)
+        # We don't have the trajectory, but at least we can rebase the height
+        prediction[:, :, 2] -= np.min(prediction[:, :, 2])
 
-        viz_limit = -1
-        viz_bitrate = 3000
-        viz_downsample = 1
-        viz_skip = 0
-        viz_size = 5
+    anim_output = {'Reconstruction': prediction}
 
-        render_animation(input_keypoints, keypoints_metadata, anim_output,
-                         dataset.skeleton(), dataset.fps(), viz_bitrate, cam['azimuth'], viz_output,
-                         limit=viz_limit, downsample=viz_downsample, size=viz_size,
-                         input_video_path=viz_video, viewport=(cam['res_w'], cam['res_h']),
-                         input_video_skip=viz_skip)
+    input_keypoints = image_coordinates(input_keypoints[..., :2], w=cam['res_w'], h=cam['res_h'])
+
+    from common.visualization import render_animation
+
+    render_animation(input_keypoints, keypoints_metadata, anim_output,
+                     dataset.skeleton(), dataset.fps(), viz_bitrate, cam['azimuth'], viz_output,
+                     limit=viz_limit, downsample=viz_downsample, size=viz_size,
+                     input_video_path=viz_video, viewport=(cam['res_w'], cam['res_h']),
+                     input_video_skip=viz_skip)
 
 
 def evaluate(test_generator, model_pos, model_traj, joints_left, joints_right, action=None, return_predictions=False,
