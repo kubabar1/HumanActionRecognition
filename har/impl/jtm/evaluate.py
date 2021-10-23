Refactor JTM code, fix bugs, add normalization and improve documentation

har/impl/jtm/evaluate.py
@@ -6,7 +6,7 @@ import torchvision.models as models
 import torchvision.transforms as transforms
 
 from .utils.JTMDataset import JTMDataset, generate_sample_images
-from ...utils.dataset_util import SetType
+from ...utils.dataset_util import SetType, normalise_skeleton_3d_batch, normalise_skeleton_3d
 from ...utils.evaluation_utils import draw_confusion_matrix
 
 
@@ -14,6 +14,13 @@ class ModelType(Enum):
     FRONT = 0
     TOP = 1
     SIDE = 2
+
+
+transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor()
+])
 
 
 def load_model(model_path, classes_count):
@@ -26,15 +33,11 @@ def load_model(model_path, classes_count):
     return jtm_model
 
 
-def evaluate_tests(classes, test_data, test_labels, jtm_model, analysed_kpts_description, image_width, image_height,
-                   model_type: ModelType, result_path='results', show_diagram=True):
+def evaluate_tests_min(classes, test_data, test_labels, jtm_model, analysed_kpts_description, image_width, image_height,
+                       model_type: ModelType, result_path='results', show_diagram=True, use_normalization=True):
+    if use_normalization:
+        test_data = normalise_skeleton_3d_batch(test_data, analysed_kpts_description['left_hip'], analysed_kpts_description['right_hip'])
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-    transform = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor()
-    ])
 
     test_data_loader = JTMDataset(test_data, test_labels, image_width, image_height, len(test_data),
                                   SetType.TEST, analysed_kpts_description, is_test=True)
@@ -45,9 +48,9 @@ def evaluate_tests(classes, test_data, test_labels, jtm_model, analysed_kpts_des
     pred = []
 
     for i, (test_img, test_lbl) in enumerate(zip(test_x, test_y)):
-        test_img_tensor = torch.unsqueeze(transform(test_img[0]), model_type.value).to(device)
+        test_img_tensor = torch.unsqueeze(transform(test_img[model_type.value]), 0).to(device)
 
-        test_output = jtm_model(test_img_tensor)
+        test_output = torch.softmax(jtm_model(test_img_tensor), 1)
         pred.append(torch.argmax(test_output).item())
 
         if torch.argmax(test_output).item() == test_lbl:
@@ -61,18 +64,75 @@ def evaluate_tests(classes, test_data, test_labels, jtm_model, analysed_kpts_des
     return test_acc / len(test_x)
 
 
-def fit(classes, data, jtm_model, analysed_kpts_description, image_width, image_height, model_type: ModelType):
+def evaluate_tests(classes, test_data, test_labels, jtm_model_front, jtm_model_top, jtm_model_side, analysed_kpts_description,
+                   image_width, image_height, result_path='results', show_diagram=True, use_normalization=True):
+    if use_normalization:
+        test_data = normalise_skeleton_3d_batch(test_data, analysed_kpts_description['left_hip'], analysed_kpts_description['right_hip'])
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    transform = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.ToTensor()
-    ])
+    test_data_loader = JTMDataset(test_data, test_labels, image_width, image_height, len(test_data),
+                                  SetType.TEST, analysed_kpts_description, is_test=True)
+
+    test_x, test_y = next(iter(test_data_loader))
+
+    test_acc = 0
+    pred = []
+
+    for i, (test_img, test_lbl) in enumerate(zip(test_x, test_y)):
+        test_img_tensor_front = torch.unsqueeze(transform(test_img[ModelType.FRONT.value]), 0).to(device)
+        test_img_tensor_top = torch.unsqueeze(transform(test_img[ModelType.TOP.value]), 0).to(device)
+        test_img_tensor_side = torch.unsqueeze(transform(test_img[ModelType.SIDE.value]), 0).to(device)
+
+        test_output_front = torch.softmax(jtm_model_front(test_img_tensor_front), 1)
+        test_output_top = torch.softmax(jtm_model_top(test_img_tensor_top), 1)
+        test_output_side = torch.softmax(jtm_model_side(test_img_tensor_side), 1)
+
+        test_output = test_output_front * test_output_top * test_output_side
+
+        pred.append(torch.argmax(test_output).item())
+
+        if torch.argmax(test_output).item() == test_lbl:
+            test_acc += 1
+
+    correct_arr = [classes[int(i)] for i in test_y]
+    predicted_arr = [classes[int(i)] for i in pred]
+
+    draw_confusion_matrix(correct_arr, predicted_arr, classes, result_path=result_path, show_diagram=show_diagram)
+
+    return test_acc / len(test_x)
+
+
+def fit_min(classes, data, jtm_model, analysed_kpts_description, image_width, image_height, model_type: ModelType, use_normalization=True):
+    if use_normalization:
+        data = normalise_skeleton_3d(data, analysed_kpts_description['left_hip'], analysed_kpts_description['right_hip'])
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     smpl_img = generate_sample_images(data, analysed_kpts_description, image_width, image_height)[model_type.value]
 
     test_img_tensor = torch.unsqueeze(transform(smpl_img), 0).to(device)
-    test_output = jtm_model(test_img_tensor)
+    test_output = torch.softmax(jtm_model(test_img_tensor), 1)
+
+    return classes[torch.argmax(test_output).item()]
+
+
+def fit(classes, data, jtm_model_front, jtm_model_top, jtm_model_side, analysed_kpts_description, image_width, image_height,
+        use_normalization=True):
+    if use_normalization:
+        data = normalise_skeleton_3d(data, analysed_kpts_description['left_hip'], analysed_kpts_description['right_hip'])
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    smpl_img_front = generate_sample_images(data, analysed_kpts_description, image_width, image_height)[ModelType.FRONT.value]
+    smpl_img_top = generate_sample_images(data, analysed_kpts_description, image_width, image_height)[ModelType.TOP.value]
+    smpl_img_side = generate_sample_images(data, analysed_kpts_description, image_width, image_height)[ModelType.SIDE.value]
+
+    test_img_tensor_front = torch.unsqueeze(transform(smpl_img_front), 0).to(device)
+    test_img_tensor_top = torch.unsqueeze(transform(smpl_img_top), 0).to(device)
+    test_img_tensor_side = torch.unsqueeze(transform(smpl_img_side), 0).to(device)
+
+    test_output_front = torch.softmax(jtm_model_front(test_img_tensor_front), 1)
+    test_output_top = torch.softmax(jtm_model_top(test_img_tensor_top), 1)
+    test_output_side = torch.softmax(jtm_model_side(test_img_tensor_side), 1)
+
+    test_output = test_output_front * test_output_top * test_output_side
 
     return classes[torch.argmax(test_output).item()]

har/impl/jtm/train.py
@@ -9,7 +9,7 @@ import torchvision.models as models
 import torchvision.transforms as transforms
 
 from .utils.JTMDataset import JTMDataset
-from ...utils.dataset_util import SetType
+from ...utils.dataset_util import SetType, normalise_skeleton_3d_batch
 from ...utils.model_name_generator import ModelNameGenerator
 from ...utils.training_utils import save_model_common, save_diagram_common, Optimizer, save_loss_common, time_since
 
@@ -26,7 +26,7 @@ def train(classes, training_data, training_labels, validation_data, validation_l
           step_size_lr=30, print_every=5, val_every=5, weight_decay=0, momentum=0.9, action_repetitions=100, results_path='results',
           model_name_suffix='', optimizer_type=Optimizer.SGD, neural_network_model=NeuralNetworkModel.ALEXNET,
           save_loss=True, save_diagram=True, save_model=True, save_model_for_inference=False, use_cache=False, show_diagram=True,
-          print_results=True, remove_cache=False, add_timestamp=True):
+          print_results=True, remove_cache=False, add_timestamp=True, use_normalization=True):
     method_name = 'jtm'
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -35,6 +35,12 @@ def train(classes, training_data, training_labels, validation_data, validation_l
         transforms.CenterCrop(224),
         transforms.ToTensor()
     ])
+
+    if use_normalization:
+        training_data = normalise_skeleton_3d_batch(training_data, analysed_kpts_description['left_hip'],
+                                                    analysed_kpts_description['right_hip'])
+        validation_data = normalise_skeleton_3d_batch(validation_data, analysed_kpts_description['left_hip'],
+                                                      analysed_kpts_description['right_hip'])
 
     model_front, model_top, model_side = get_neural_network_models(neural_network_model, classes, device)
 
@@ -221,6 +227,7 @@ def train(classes, training_data, training_labels, validation_data, validation_l
         .add_step_size_lr(step_size_lr) \
         .add_gamma_step_lr(gamma_step_lr) \
         .add_neural_network_model(neural_network_model.name) \
+        .add_is_normalization_used(use_normalization) \
         .generate()
 
     if save_model:

har/impl/jtm/utils/jtm.py
@@ -7,12 +7,11 @@ from PIL import Image
 from scipy.spatial import distance
 
 
-def jtm(positions_w, positions_h, image_width, image_height, analysed_kpts_left, analysed_kpts_right, L=1, s_min=0, s_max=1,
+def jtm(positions_w, positions_h, image_width, image_height, analysed_kpts_left, L=1, s_min=0, s_max=1,
         b_min=0, b_max=1):
     img = np.zeros((image_height, image_width, 3))
     frames_count = len(positions_w)
     kpts_count = len(positions_w[0])
-    all_analysed_kpts = analysed_kpts_left + analysed_kpts_right
     hue = np.zeros([frames_count])
     v = np.zeros([frames_count, kpts_count])
     pos_tmp = np.stack([positions_w, positions_h], axis=2)